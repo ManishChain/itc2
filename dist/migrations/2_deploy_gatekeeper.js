26a9c7a9b0af4398796d6b1a90348ed08c45d270
gyan-sharma <gyanendra@settlemint.com>
2021-02-09 21:13:18 +0530

    deploy

--- dist/migrations/2_deploy_gatekeeper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GateKeeper = artifacts.require('GateKeeper.sol');
-module.exports = async (deployer, network, accounts) => deployer.deploy(GateKeeper);
-//# sourceMappingURL=2_deploy_gatekeeper.js.map
